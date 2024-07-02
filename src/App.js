e3f322fd9ad1cd3e1120073f86763db011e8e90a
nissy325 <240noadress+github@gmail.com>
2024-07-02 09:03:54 +0900

    fix: description

--- src/App.js
+++ src/App.js
@@ -90,7 +90,12 @@ export default function Game() {
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0) {
-      description = 'Go to move #' + move;
+      if (move === currentMove) {
+        description = 'You are at move #' + move;
+
+      } else {
+        description = 'Go to move #' + move;
+      }
     } else {
       description = 'Go to game start';
     }
